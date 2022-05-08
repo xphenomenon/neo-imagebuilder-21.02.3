1b78b9592a0a6a90addcdd9db3dd52f766bd00b0
Joseph Chen <joseph.chen@realtek.com>
2022-05-08 17:13:28 +0800

    update

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ make image FILES="files" PROFILE=friendlyarm_nanopi-neo \
     DISABLED_SERVICES="dnsmasq odhcpd uhttpd" \
     PACKAGES="kmod-cfg80211 kmod-mac80211 iperf iperf3 autossh openvpn-openssl \
     sshfs scapy tcpreplay wpad-openssl tcpdump rpcapd kmod-usb-core usbutils gdb gdbserver wireless-tools zram-swap \
-    wpa-cli hostapd-utils dmesg coreutils-tee luci usb-modeswitch"
+    wpa-cli hostapd-utils dmesg coreutils-tee luci usb-modeswitch \
+    kmod-rtw88-usb"
