16f0cb3e17200b164967d394699321b64cba00a6
Joseph Chen <joseph.chen@realtek.com>
2022-05-09 08:27:23 +0800

    for vscode

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ make image FILES="files" PROFILE=friendlyarm_nanopi-neo \
     PACKAGES="kmod-cfg80211 kmod-mac80211 iperf iperf3 autossh openvpn-openssl \
     sshfs scapy tcpreplay wpad-openssl tcpdump rpcapd kmod-usb-core usbutils gdb gdbserver wireless-tools zram-swap \
     wpa-cli hostapd-utils dmesg coreutils-tee luci usb-modeswitch \
-    kmod-rtw88-usb kmod-rtl8812au-ac aircrack-ng kmod-rtl8188eu mdk4\
-    kmod-rtl8192fu arp-scan arp-scan-database nmap kmod-mt7601u kmod-rtl8821cu"
+    kmod-rtw88-usb kmod-rtl8812au-ac aircrack-ng kmod-rtl8188eu mdk4 \
+    kmod-rtl8192fu arp-scan arp-scan-database nmap kmod-mt7601u kmod-rtl8821cu \
+    openssh-sftp-server"
