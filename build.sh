98ad7ed3c7b3b5fab119ec30ee5af4068aa6dee2
Joseph Chen <joseph.chen@realtek.com>
2022-05-10 16:34:23 +0800

    dhclient

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ make image FILES="files" PROFILE=friendlyarm_nanopi-neo \
     wpa-cli hostapd-utils dmesg coreutils-tee luci usb-modeswitch \
     kmod-rtw88-usb kmod-rtl8812au-ac aircrack-ng kmod-rtl8188eu mdk4 \
     kmod-rtl8192fu arp-scan arp-scan-database nmap kmod-mt7601u kmod-rtl8821cu \
-    openssh-sftp-server tmux vim git rsync make"
+    openssh-sftp-server tmux vim git rsync make isc-dhcp-client-ipv4"
