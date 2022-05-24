2d82c668e65f4bbe3386b48dd1bf1bea42b312fc
Joseph Chen <joseph.chen@realtek.com>
2022-05-24 19:07:56 +0800

    for wifi

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ make image FILES="files" PROFILE=friendlyarm_nanopi-neo \
     kmod-rtw88-usb kmod-rtl8812au-ac aircrack-ng kmod-rtl8188eu mdk4 \
     kmod-rtl8192fu arp-scan arp-scan-database nmap kmod-mt7601u kmod-rtl8821cu \
     openssh-sftp-server tmux vim git rsync make isc-dhcp-client-ipv4 kmod-rtl8192cu \
-    kmod-mt76x2u kmod-rtl8852bu perl iwinfo"
+    kmod-mt76x2u kmod-rtl8852bu perl iwinfo "
